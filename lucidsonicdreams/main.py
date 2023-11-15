be77bd9d2369d10c458c9d7b105fb638c9e795c1
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2023-11-14 19:50:03 -1000

    Update main.py

--- lucidsonicdreams/main.py
+++ lucidsonicdreams/main.py
@@ -164,31 +164,31 @@ class LucidSonicDream:
         self.chrom_class, self.pitches_sorted = chrom_class, pitches_sorted
 
 
-def transform_classes(self):
-    '''Transform/assign value of classes'''
-
-
-# If model does not use classes, simply return list of 0's
-if self.num_possible_classes == 0:
-    self.classes = [0] * 12
-
-else:
-
-    # If list of classes is not provided, generate a random sample
-    if self.classes is None:
-        self.classes = random.sample(range(self.num_possible_classes),
-                                     min([self.num_possible_classes, 12]))
-
-    # If length of list < 12, repeat list until length is 12
-    if len(self.classes) < 12:
-        self.classes = (self.classes *
-                        int(np.ceil(12 / len(self.classes))))[:12]
-
-    # If dominant_classes_first is True, sort classes accordingly
-    if self.dominant_classes_first:
-        self.classes = [
-            self.classes[i] for i in np.argsort(self.pitches_sorted)
-        ]
+    def transform_classes(self):
+        '''Transform/assign value of classes'''
+    
+    
+        # If model does not use classes, simply return list of 0's
+        if self.num_possible_classes == 0:
+            self.classes = [0] * 12
+        
+        else:
+        
+            # If list of classes is not provided, generate a random sample
+            if self.classes is None:
+                self.classes = random.sample(range(self.num_possible_classes),
+                                             min([self.num_possible_classes, 12]))
+        
+            # If length of list < 12, repeat list until length is 12
+            if len(self.classes) < 12:
+                self.classes = (self.classes *
+                                int(np.ceil(12 / len(self.classes))))[:12]
+        
+            # If dominant_classes_first is True, sort classes accordingly
+            if self.dominant_classes_first:
+                self.classes = [
+                    self.classes[i] for i in np.argsort(self.pitches_sorted)
+                ]
 
     def update_motion_signs(self):
         '''Update direction of noise interpolation based on truncation value'''
